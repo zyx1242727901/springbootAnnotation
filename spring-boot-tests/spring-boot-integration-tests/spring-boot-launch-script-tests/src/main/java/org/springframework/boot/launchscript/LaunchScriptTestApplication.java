f8e95c8aa6772096749790c5715c0998fb75f95f
zhangyuxiao <zhangyuxiao@daokoudai.com>
2020-07-03 19:23:58 +0800

    启动类注释

--- spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/main/java/org/springframework/boot/launchscript/LaunchScriptTestApplication.java
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/main/java/org/springframework/boot/launchscript/LaunchScriptTestApplication.java
@@ -21,7 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LaunchScriptTestApplication {
-
+	//Starter Enter
 	public static void main(String[] args) {
 		SpringApplication.run(LaunchScriptTestApplication.class, args);
 	}
